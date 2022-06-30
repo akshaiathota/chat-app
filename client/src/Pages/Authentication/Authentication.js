5760f21994838260e42a675d1087241721d5fa6e
saisuryajss <saisuryajss@gmail.com>
2022-06-30 21:30:21 +0530

    made login page responsive

--- client/src/Pages/Authentication/Authentication.js
+++ client/src/Pages/Authentication/Authentication.js
@@ -26,19 +26,21 @@ function Authentication() {
     }
 
     return (
-        <div className={(toggleInput ? 'height-small' : 'height-large') + ' authentication'}>
-            <div className='a-title'>
-                <h1>Just An Other Chat Application</h1>
-            </div>
-            <div className='a-body'>
-                <div className='a-headers'>
-                    <ul>
-                        <li onClick={handleClick} className={toggleInput ? 'highlight' : ''}>Log In</li>
-                        <li onClick={handleClick} className={toggleInput ? '' : 'highlight'}>Sign Up</li>
-                    </ul>
+        <div className='login-signup-page'>
+            <div className={(toggleInput ? 'height-small' : 'height-large') + ' authentication'}>
+                <div className='a-title'>
+                    <h1>Just An Other Chat Application</h1>
                 </div>
-                <div className='a-logInAndSignUp'>
-                    {toggleInput ? <Login /> : <Signup />}
+                <div className='a-body'>
+                    <div className='a-headers'>
+                        <ul>
+                            <li onClick={handleClick} className={toggleInput ? 'highlight' : ''}>Log In</li>
+                            <li onClick={handleClick} className={toggleInput ? '' : 'highlight'}>Sign Up</li>
+                        </ul>
+                    </div>
+                    <div className='a-logInAndSignUp'>
+                        {toggleInput ? <Login /> : <Signup />}
+                    </div>
                 </div>
             </div>
         </div>
